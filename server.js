acfdaeb9204eac6416393cda4356581442de9fc5
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-05-22 17:18:49 -0300

    fixes: gblesss

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./')
 const day = new Date()
 const currentDay = day.toLocaleString()
-const port = process.env.PORT || 3333
+const port = process.env.PORT ?? 3333
 app.listen(port, () => console.log(`server running on port ${port} at ${currentDay}`))
