a5defab4f96448785dc6ce258d7221f2716d8aad
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-05-24 10:53:17 -0300

    teste deploy

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,14 @@ const AccessController = require('./app/controllers/accessController')
 
 routes.post("/cadastros", RegisterController.store)
 routes.post("/access", AccessController.update)
-routes.get("/access", AccessController.index)
+// routes.get("/access", AccessController.index)
+
+routes.get("/access", (req, res) => {
+    return res.json("Hello World")
+})
 
 routes.get("/", (req, res) => {
     return res.json("Hello World")
 })
+
 module.exports = routes
